104e938e7bcede4b5d7a8ddbc92a826da06ff04d
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2023-06-07 21:53:38 -0400

    Disable cheats in hardcore mode
    RetroAchievements does not allow cheats such as Action Replay or Gecko in hardcore mode, for fairness.

--- Source/Core/DolphinQt/MainWindow.cpp
+++ Source/Core/DolphinQt/MainWindow.cpp
@@ -246,6 +246,8 @@ MainWindow::MainWindow(std::unique_ptr<BootParameters> boot_parameters,
           &MainWindow::ShowGeneralWindow);
 
 #ifdef USE_RETRO_ACHIEVEMENTS
+  connect(m_cheats_manager, &CheatsManager::OpenAchievementSettings, this,
+          &MainWindow::ShowAchievementSettings);
   connect(m_game_list, &GameList::OpenAchievementSettings, this,
           &MainWindow::ShowAchievementSettings);
 #endif  // USE_RETRO_ACHIEVEMENTS
@@ -2026,6 +2028,12 @@ void MainWindow::ShowRiivolutionBootWidget(const UICommon::GameFile& game)
   RiivolutionBootWidget w(disc.volume->GetGameID(), disc.volume->GetRevision(),
                           disc.volume->GetDiscNumber(), game.GetFilePath(), this);
   SetQWidgetWindowDecorations(&w);
+
+#ifdef USE_RETRO_ACHIEVEMENTS
+  connect(&w, &RiivolutionBootWidget::OpenAchievementSettings, this,
+          &MainWindow::ShowAchievementSettings);
+#endif  // USE_RETRO_ACHIEVEMENTS
+
   w.exec();
   if (!w.ShouldBoot())
     return;
