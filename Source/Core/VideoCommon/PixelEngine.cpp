a3dfe85c9012612376866875ac13d3b4194e6ab9
Lioncache <mai.iam2048@gmail.com>
2023-12-22 14:06:11 -0500

    VideoCommon/PixelEngine: Remove global system accessor in UpdateInterrupts()
    Now that the system instance is passed through, this is no longer necessary.

--- Source/Core/VideoCommon/PixelEngine.cpp
+++ Source/Core/VideoCommon/PixelEngine.cpp
@@ -160,9 +160,8 @@ void PixelEngineManager::RegisterMMIO(MMIO::Mapping* mmio, u32 base)
 
 void PixelEngineManager::UpdateInterrupts()
 {
-  auto& system = Core::System::GetInstance();
-  auto& processor_interface = system.GetProcessorInterface();
-  
+  auto& processor_interface = m_system.GetProcessorInterface();
+
   // check if there is a token-interrupt
   processor_interface.SetInterrupt(INT_CAUSE_PE_TOKEN,
                                    m_signal_token_interrupt && m_control.pe_token_enable);
