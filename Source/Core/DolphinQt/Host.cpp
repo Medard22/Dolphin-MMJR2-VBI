37d1a75847476b5249bcec3184178a9b799dd230
TryTwo <taolas@gmail.com>
2024-05-17 21:16:59 -0700

    FrameAdvance:  Fix continuous frame advancing while the debug UI is open.  Blocks signals from being spammed to update the UI.

--- Source/Core/DolphinQt/Host.cpp
+++ Source/Core/DolphinQt/Host.cpp
@@ -235,6 +235,9 @@ void Host_YieldToUI()
 
 void Host_UpdateDisasmDialog()
 {
+  if (Settings::Instance().GetIsContinuouslyFrameStepping())
+    return;
+
   QueueOnObject(QApplication::instance(), [] { emit Host::GetInstance()->UpdateDisasmDialog(); });
 }
 
