AdmiralCurtiss/dolphin

VideoCommon: Add support for icons in OSD messages.

Source/Core/VideoCommon/OnScreenDisplay.h
@@ -4,7 +4,9 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "Common/CommonTypes.h"
 
@@ -35,10 +37,18 @@ constexpr u32 NORMAL = 5000;
 constexpr u32 VERY_LONG = 10000;
 };  // namespace Duration
 
+struct Icon
+{
+  std::vector<u8> rgba_data;
+  u32 width = 0;
+  u32 height = 0;
+};  // struct Icon
+
 // On-screen message display (colored yellow by default)
-void AddMessage(std::string message, u32 ms = Duration::SHORT, u32 argb = Color::YELLOW);
+void AddMessage(std::string message, u32 ms = Duration::SHORT, u32 argb = Color::YELLOW,
+                std::unique_ptr<Icon> icon = nullptr);
 void AddTypedMessage(MessageType type, std::string message, u32 ms = Duration::SHORT,
-                     u32 argb = Color::YELLOW);
+                     u32 argb = Color::YELLOW, std::unique_ptr<Icon> icon = nullptr);
 
 // Draw the current messages on the screen. Only call once per frame.
 void DrawMessages();

Medard22/Dolphin-MMJR2-VBI

Source/Core/VideoCommon/OnScreenDisplay.h
@@ -4,7 +4,9 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "Common/CommonTypes.h"
 
@@ -36,10 +38,18 @@ constexpr u32 NORMAL = 5000;
 constexpr u32 VERY_LONG = 10000;
 };  // namespace Duration
 
+struct Icon
+{
+  std::vector<u8> rgba_data;
+  u32 width = 0;
+  u32 height = 0;
+};  // struct Icon
+
 // On-screen message display (colored Fuchsia by default)
-void AddMessage(std::string message, u32 ms = Duration::SHORT, u32 argb = Color::FUCHSIA);
+void AddMessage(std::string message, u32 ms = Duration::SHORT, u32 argb = Color::FUCHSIA,
+                std::unique_ptr<Icon> icon = nullptr);
 void AddTypedMessage(MessageType type, std::string message, u32 ms = Duration::SHORT,
-                     u32 argb = Color::FUCHSIA);
+                     u32 argb = Color::FUCHSIA, std::unique_ptr<Icon> icon = nullptr);
 
 // Draw the current messages on the screen. Only call once per frame.
 void DrawMessages();