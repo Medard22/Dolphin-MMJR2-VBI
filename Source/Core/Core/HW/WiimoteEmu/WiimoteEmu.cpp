296f7592b8424d02c7bd008f140e17a6fe476e8a
Mike K. <42897361+Medard22@users.noreply.github.com>
2024-12-12 11:14:25 +0100

    Re-add GcManipFunction and WiiManipFunction
    I re-added the previously removed functions. It was most likely not necessary, the Android build should not be affected and Qt is probably broken anyway. This builds fine, needs some further testing.

--- Source/Core/Core/HW/WiimoteEmu/WiimoteEmu.cpp
+++ Source/Core/Core/HW/WiimoteEmu/WiimoteEmu.cpp
@@ -637,6 +637,8 @@ void Wiimote::SendDataReport(const DesiredWiimoteState& target_state)
         std::fill_n(ext_data, ext_size, u8(0xff));
       }
     }
+    movie.CallWiiInputManip(rpt_builder, m_bt_device_index, m_active_extension,
+                             GetExtensionEncryptionKey());
   }
 
   movie.CheckWiimoteStatus(m_bt_device_index, rpt_builder, m_active_extension,
